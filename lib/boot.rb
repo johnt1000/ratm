c9c8323bacb84f092ab9f3528d281f6e65c3ce2a
Jônatas Rodrigues <jonatas.rodriguess@gmail.com>
2019-05-16 10:00:49 -0300

    Added boot class

---
+++ lib/boot.rb
@@ -0,0 +1,16 @@
+class Boot
+    def initialize
+        puts "Starting system...\n".dark_purple
+    end
+
+    def update_gems
+        puts "Update gems...\n".dark_purple
+        zipbruteforce
+        puts "\nFinished gem update\n".dark_purple
+    end
+
+    def zipbruteforce
+        puts 'ZipBruteForce'.blue
+        puts `cd vendor/zipbruteforce/ && bundle install`.green
+    end
+end

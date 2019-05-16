aabd8703daafd179fcc91f18170b3e21348ab118
Jônatas Rodrigues <jonatas.rodriguess@gmail.com>
2019-05-16 10:00:29 -0300

    Updated console

--- lib/console.rb
+++ lib/console.rb
@@ -7,17 +7,26 @@ trap('INT', 'SIG_IGN') # X -> Ctrl+C
 $ctx = 'main' # console context
 @ratm = Ratm.new
 
+require_relative 'boot'
+
+boot = Boot.new
+boot.update_gems
+
 # commands
 CMDS = [
-    'help'.yellow.concat("\t\tList all commands."),
+    'zipbf'.yellow.concat("\tBrute force for zip files."),
     'ls'.yellow.concat("\t\tList files."),
+    'help'.yellow.concat("\t\tList all commands."),
     'version'.yellow.concat("\t\tSystem version."),
     'exit'.yellow.concat("\t\tExit or quit."),
 ].sort
 
 completion = 
     proc do |str|
-        case 
+        case
+        when Readline.line_buffer =~ /zipbf.*/i
+            params = Readline.line_buffer.split(/ /)
+            puts `ruby vendor/zipbruteforce/zipbruteforce #{params[1]} #{params[2]}`
         when Readline.line_buffer =~ /help.*/i
             puts "\nAvailable commands:\n".blue + "#{CMDS.join("\n")}"
         when Readline.line_buffer =~ /rubyfu.*/i

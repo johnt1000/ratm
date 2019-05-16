0f145c70df4a956debbd5759c6f90b76281a3b69
Jônatas Rodrigues <jonatas.rodriguess@gmail.com>
2019-05-16 11:29:23 -0300

    Update plugins

--- lib/boot.rb
+++ lib/boot.rb
@@ -4,14 +4,20 @@ class Boot
   end
 
   def update_gems
-    puts "Update gems...\n".dark_purple
-    zipbruteforce
-    puts "\nFinished gem update\n".dark_purple
+    puts "Download and update plugins...\n".dark_purple
+    download_zipbruteforce
+    puts "\nFinished\n".dark_purple
   end
 
   private
+    def download_zipbruteforce
+      puts "> ZipBruteForce \n".blue
+      puts "Download...\n".yellow
+      puts `cd ./vendor/ && git clone https://github.com/johnt1000/zipbruteforce.git`.green
+      bundle_zipbruteforce
+    end
     def bundle_zipbruteforce
-      puts 'ZipBruteForce'.blue
+      puts 'Update gems...'.yellow
       puts `cd vendor/zipbruteforce/ && bundle install`.green
     end
 end
